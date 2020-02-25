ea33770a12c845ce15f8f99b4ad1f03d9ac8bd29
Benjamin Fuhro <befuhro@z3r7p4.le-101.fr>
2020-02-25 18:05:20 +0100

    test(client): socket coverage 100%

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+module.exports = {
+    "verbose": true,
+    "transform": {
+        "^.+\\.js$": "babel-jest",
+        "^.+\\.jsx$": "babel-jest",
+    },
+    "moduleNameMapper":{
+    },
+    "globals": {
+        "NODE_ENV": "test"
+    },
+    "moduleFileExtensions": [
+        "js",
+        "jsx",
+        "json"
+    ]
+};
--- test/client/utils/socketConnection.test.js
+++ test/client/utils/socketConnection.test.js
@@ -1,13 +1,39 @@
+import ioClient from 'socket.io-client';
+
+import {roomConnection} from "../../../src/client/utils/socketConnection";
+
 const server = require('http').createServer();
 const io = require('socket.io')(server);
+const room = io.of("/room");
+const socketClient = ioClient('localhost:8080/room');
 
-io.on('connection', client => {
-    client.on('event', data => console.log(data));
-    client.on('disconnect', () => console.log('disconnect'));
-});
 
-server.listen(8080);
+describe('socket', () => {
 
-test('', () => {
+    beforeAll(done => {
+        server.listen(8080);
+        done();
+    });
 
+    afterAll(done => {
+        server.close();
+        if (socketClient.connected) socketClient.disconnect();
+        done();
+    });
+
+    it('opponent connection', done => {
+        roomConnection(socketClient, data => {
+            console.log(data);
+            if (data.type === 'CHANGE_INTERVAL') done();
+        });
+
+        room.on('connect', socketServer => {
+
+            socketServer.emit('opponent connection', {players: ['test'], leaderName: 'hada'});
+            socketServer.emit('opponent disconnection', {players: ['test'], leaderName: 'hada'});
+            socketServer.emit('new spectrum', {});
+            socketServer.emit('launch party');
+            socketServer.emit('update interval', 150);
+        });
+    });
 });
